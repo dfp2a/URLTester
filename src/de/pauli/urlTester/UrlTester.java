efbfce0cd79df595e474333dbb8f7bf89a3acb61
David Florian Pauli <dfpauli@s-und-n.de>
2018-11-17 13:58:57 +0100

    Änderung auf neuen Filewriter

--- src/de/pauli/urlTester/UrlTester.java
+++ src/de/pauli/urlTester/UrlTester.java
@@ -1,5 +1,8 @@
 package de.pauli.urlTester;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -21,7 +24,6 @@ import jdk.incubator.http.HttpResponse;
 
 public class UrlTester {
 	static boolean running = true;
-	 private static final Logger log = Logger.getLogger( UrlTester.class.getName() );
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {
@@ -32,47 +34,64 @@ public class UrlTester {
 		System.out.println(aufforderung);
 		String eigegebeneURL = scanner.nextLine();
 		
-		Handler handler =  new FileHandler( "log.txt" );
-		handler.setLevel( Level.FINEST );
-		log.addHandler( handler );
 
 
-		SimpleDateFormat ft = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss");
+		SimpleDateFormat ft = new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss"), ft2 = new SimpleDateFormat("dd-MMM-yyyy hh-mm-ss");;
 
 		final URL url = new URL(eigegebeneURL);
 
 		System.out.println(String.format(
 				"%s  wird überwacht, drücken Sie eine beliebige Taste um das Programm zu beenden...", eigegebeneURL));
-
+		File f= new File(ft2.format(new Date())+".log");
+		
 		Runnable r = (new Runnable() {
 
 			@Override
 			public void run() {
 				
-				Date currentdate, olddate = new Date(0);
-				while (true) {
-					currentdate = new Date();
-					int status = 1000;
-					if (currentdate.getTime() > olddate.getTime() + 30 * 1000) {
-						olddate=currentdate;
-						String timestring = ft.format(currentdate);
+				Date currentdate=new Date(), olddate = new Date(0);
+				FileWriter fw=null;BufferedWriter br=null;
+				try {
+				
+					fw = new FileWriter(f,true);
+					 br = new BufferedWriter(fw);
+					
+					//while (true) {
+						currentdate = new Date();
+						int status = 1000;
+						if (currentdate.getTime() > olddate.getTime() + 30 * 1000) {
+							olddate=currentdate;
+							String timestring = ft.format(currentdate);
 
+							
+							try {
+								HttpURLConnection con = (HttpURLConnection) url.openConnection();
+								con.setRequestMethod("GET");
+								con.setConnectTimeout(10000);
+								status = con.getResponseCode();
+
+							} catch (Exception e) {
+								// System.err.println(e.toString());
+							}
+							String timereport=String.format("%s : %s -> %s", timestring, eigegebeneURL,
+									((status > 400) ? "nicht erreichbar" : "erreichbar"));
+							System.out.println(timereport);
+							br.append(timereport);
+							if(br!=null) {br.close();}
+							if(fw!=null) {fw.close();}
 						
-						try {
-							HttpURLConnection con = (HttpURLConnection) url.openConnection();
-							con.setRequestMethod("GET");
-							con.setConnectTimeout(10000);
-							status = con.getResponseCode();
-
-						} catch (Exception e) {
-							// System.err.println(e.toString());
-						}
-						String timereport=String.format("%s : %s -> %s", timestring, eigegebeneURL,
-								((status > 400) ? "nicht erreichbar" : "erreichbar"));
-						System.out.println(timereport);
-						log.info(timereport);
+						//}
 					}
+					
+				} catch (IOException e1) {
+					
+					e1.printStackTrace();
+				}finally {
+					
 				}
+				
+				
+			
 
 			}
 		});
@@ -82,7 +101,9 @@ public class UrlTester {
 		InputStream inputStream = System.in;
 		try {
 			inputStream.read();
+			
 			t.stop();
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
